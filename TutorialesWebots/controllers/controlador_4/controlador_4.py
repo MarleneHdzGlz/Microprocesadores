09d39012bb43ff98573af13a5782c228d975bf20
Marlene Hernandez Gonzalez <marlene2233520224@gmail.com>
2026-09-15 17:30:11 -0600

    Add files via upload

---
+++ TutorialesWebots/controllers/controlador_4/controlador_4.py
@@ -0,0 +1,53 @@
+"""controlador_4 controller."""
+
+from controller import Robot, DistanceSensor, Motor
+
+robot = Robot()
+
+TIME_STEP = int(robot.getBasicTimeStep())
+
+MAX_SPEED = 6.28
+
+ps = []
+psNames = [
+    'ps0', 'ps1', 'ps2', 'ps3',
+    'ps4', 'ps5', 'ps6', 'ps7'
+]
+
+for i in range(8):
+    ps.append(robot.getDevice(psNames[i]))
+    ps[i].enable(TIME_STEP)
+    
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)    
+
+while robot.step(TIME_STEP) != -1:
+
+    psValues = []
+    for i in range(8):
+        psValues.append(ps[i].getValue())
+        
+    right_obstacle = psValues[0] > 80.0 or psValues[1] > 80.0 or psValues[2] > 80.0
+    left_obstacle = psValues[5] > 80.0 or psValues[6] > 80.0 or psValues[7] > 80.0
+
+    leftSpeed  = 0.5 * MAX_SPEED
+    rightSpeed = 0.5 * MAX_SPEED
+
+    if left_obstacle:
+        # turn right
+        leftSpeed  = 0.5 * MAX_SPEED
+        rightSpeed = -0.5 * MAX_SPEED
+    elif right_obstacle:
+        # turn left
+        leftSpeed  = -0.5 * MAX_SPEED
+        rightSpeed = 0.5 * MAX_SPEED
+    # write actuators inputs
+    leftMotor.setVelocity(leftSpeed)
+    rightMotor.setVelocity(rightSpeed)
+    
+    pass
+
